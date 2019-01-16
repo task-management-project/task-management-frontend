76e549e7727f305304e1fda44e5259c7d758d7be
Jamie Kinkead <jkinkead@gmail.com>
2019-01-16 14:09:26 -0800

    focus page working

--- src/components/FocusCard.js
+++ src/components/FocusCard.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import { Tile, Heading, Button } from 'react-bulma-components'
+import { Tile, Heading, } from 'react-bulma-components'
 
 
 
 
 
-export default function TaskCard({name, description, thoughts, deleteTask}) {
+export default function TaskCard({name, description, thoughts,}) {
     return (
 
         <Tile kind="parent">
-            <Tile renderAs="article" kind="child" notification color="success">
+            <Tile renderAs="article" kind="child" notification color="primary">
                 <div className="content">
                     <Heading>{name}</Heading>
                     <Heading subtitle>{description}</Heading>
                     <Heading subtitle>{thoughts}</Heading>
                     <div className="content" />
                 </div>
-                <Button onClick={deleteTask}>Delete</Button>
             </Tile>
         </Tile >
 
     )
 }
-
-
-
 
--- src/components/MemberFocus.js
+++ src/components/MemberFocus.js
@@ -1,38 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Box, Heading} from 'react-bulma-components'
-import TaskCard from './TaskCard'
+import { Box, Heading } from 'react-bulma-components'
+import FocusCard from './FocusCard'
+import {getMemberTasks} from '../actions/tasks'
 
 //adjust so displays active task
 
-const MemberFocus = () => (
-    <div>
-        <Box>
-            <Heading>Task Focus Zone</Heading>
-            <TaskCard/>
-        </Box>
-        
-      
-      <Link to={'./memberdash'}>
-        return to dashboard
+class MemberFocus extends Component {
+    constructor(props) {
+        super(props)
+        this.state = null
+    }
+
+    componentDidMount = () => {
+        this.props.getMemberTasks(this.props.userId)
+    
+       
+      }
+
+
+    render() {
+
+        return (
+            <div>
+                <Box>
+                    <Heading>Task Focus Zone</Heading>
+                    {
+                        this.props.tasks.filter(task => {
+                            return task.isFocus
+                        }).map(task => <FocusCard key={task.id} {...task} />)
+                    }
+                </Box>
+
+
+                <Link to={'./memberdash'}>
+                    return to dashboard
       </Link>
 
-    </div>
-
-)
-
+            </div>
+        )
+    }
+}
 
 
 
 
 const mapStateToProps = state => ({
+    tasks: state.tasks,
     userId: state.authentication.user.id
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-
+getMemberTasks: getMemberTasks
 }, dispatch)
 
 export default connect(
