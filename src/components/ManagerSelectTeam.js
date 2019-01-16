42dc9f7593b6fb36fb4c9ee060e737f1dd651d32
Michael Spraggins <michael.c.spraggins@gmail.com>
2019-01-16 14:04:51 -0800

    Implemented user form. Currently no teams routes. Building routes then continuing work.

--- src/components/ManagerSelectTeam.js
+++ src/components/ManagerSelectTeam.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Box, Heading, Dropdown } from 'react-bulma-components'
+import { Box, Heading, Dropdown, Form } from 'react-bulma-components'
 const { Item } = Dropdown
-
+const { Label, Field, Input, Control } = Form
 
 class ManagerSelectTeam extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            teamName: 'placeholder team name',
-            teamDescription: 'placeholder team description',
+            teamName: '',
+            teamDescription: '',
             teamMembers: ["person three", "person four", "person seven"]
         }
     }
 
+    handleChange = (e) => this.setState({[e.target.name]: e.target.value})
 
     render() {
         return (
             <div>
             <Box>
-                <Heading>{this.state.teamName}</Heading>
-                <Heading>{this.state.teamDescription}</Heading>
+                <form onSubmit={null}>
+                    <Field>
+                        <Label>Team Name</Label>
+                        <Control>
+                            <Input onChange={this.handleChange} color="success" type="text" name="teamName" value={this.state.teamName} />
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label>Team Description</Label>
+                        <Control>
+                            <Input onChange={this.handleChange} color="success" type="text" name="teamDescription" value={this.state.teamDescription} />
+                        </Control>
+                    </Field>
+                </form>
             </Box>
         <Box>
             <Heading>Assemble Your Team</Heading>
