eabee2d44d053d2773a56ef1782db3ae9687b58c
Jamie Kinkead <jkinkead@gmail.com>
2019-01-17 11:02:59 -0800

    manager dash and member cards started

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import {Card} from 'react-bulma-components'
+import { Tile, Heading, Button } from 'react-bulma-components'
 
 
 
-export default function MemberCard() {
+
+
+export default function MemberCard({member, name, description}) {
     return (
-        <Card>
-            Member Here
-            Task Name
-            Task Description
-        </Card>
+
+        <Tile kind="parent">
+            <Tile renderAs="article" kind="child" notification color="success">
+                <div className="content">
+                    <Heading>{member}</Heading>
+                    <Heading subtitle>{name}</Heading>
+                    <Heading subtitle>{description}</Heading>
+                   
+                    <div className="content" />
+                </div>
+                <Button >Remove</Button>
+            </Tile>
+        </Tile >
+
     )
 }
+
+
